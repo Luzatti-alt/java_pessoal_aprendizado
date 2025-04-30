9cb91983bcbdb4cf4a50b96b12d3f2459be6017a
Luzatti-alt <lucasguzatti4@gmail.com>
2025-04-30 14:44:09 -0300

    Delete java_swing/Troca_janelas(3+_arquivos) /Deste_jeito_dá_para_criar_um_projeto_legal directory

--- java_swing/Troca_janelas(3+_arquivos) /Deste_jeito_dá_para_criar_um_projeto_legal/Main.java
+++
@@ -1,6 +0,0 @@
-package Troca_janelas;
-public class Main {
-    public static void main(String[]args){
-        LaunchPage launchPage = new LaunchPage();//utiliza o construtor launchPage criado em LAunchPage.java
-    }   
-}
