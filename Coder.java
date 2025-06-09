e9588cc78fe9fb8bdf2ba5c5729fea427b2ecb8d
Luzatti-alt <lucasguzatti4@gmail.com>
2025-06-09 11:38:52 -0300

    Update Coder.java
    mudar tamanho do editor

--- Coder.java
+++ Coder.java
@@ -7,6 +7,7 @@ public class Coder extends JFrame implements ComponentListener{
     JPanel fundo = new JPanel();
     JPanel arquivos = new JPanel();
     JPanel terminal = new JPanel();
+    JPanel editor = new JPanel();
     public Coder(){
         this.setMinimumSize(new Dimension(500,500));
         this.add(fundo);
@@ -14,6 +15,7 @@ public class Coder extends JFrame implements ComponentListener{
         fundo.setBackground(Color.BLACK);
         fundo.add(terminal);
         fundo.add(arquivos);
+        fundo.add(editor);
         this.addComponentListener(new ComponentAdapter() {
         @Override
         public void componentResized(ComponentEvent e) {
@@ -22,9 +24,11 @@ public class Coder extends JFrame implements ComponentListener{
         terminal.setLocation(largura_atual, altura_atual);
         terminal.setBounds(0+(largura_atual/3),altura_atual-(altura_atual/3),largura_atual-(altura_atual/3),altura_atual/3);
         arquivos.setBounds(0,0,largura_atual/3, altura_atual);
+        editor.setBounds(0+(largura_atual/3),0,0,0);
         fundo.revalidate();
         fundo.repaint();
     }});
+        editor.setBackground(Color.green);
         arquivos.setBackground(Color.blue);
         terminal.setBackground(Color.red);
         this.setVisible(true);
